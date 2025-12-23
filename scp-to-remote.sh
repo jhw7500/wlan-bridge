0ea1ddfa642e7bcaa3ebcdbf6d67c27fd7d4b353
hwjo <hwjo@cantops.biz>
2025-12-23 15:15:48 +0900

    workflow

---
+++ scp-to-remote.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+REMOTE="root@192.168.0.110"
+scp dumb/dumb.c "$REMOTE":/home/root/wlan-bridge/dumb/dumb.c
--- scripts/scp_to_target.sh
+++ scripts/scp_to_target.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-scp Makefile root@192.168.0.110:/root/claude
-scp dumb.c root@192.168.0.110:/root/claude
-scp dumb-tpacket.c root@192.168.0.110:/root/claude
+scp ../dumb/Makefile root@192.168.0.110:/root/claude
+scp ../dumb/dumb.c root@192.168.0.110:/root/claude
+scp ../dumb/dumb-tpacket.c root@192.168.0.110:/root/claude
 scp setup-irq-affinity.sh root@192.168.0.110:/root/claude
+scp optimize-for-udp.sh root@192.168.0.110:/root/claude
